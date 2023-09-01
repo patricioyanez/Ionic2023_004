94fb7e5b259c4c6dbaa43722a069dd1180c3d796
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-01 12:05:22 -0400

    clase 4

--- miAplicacion04/src/app/siete/siete.page.ts
+++ miAplicacion04/src/app/siete/siete.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-siete',
@@ -11,12 +12,20 @@ export class SietePage implements OnInit {
   n1 = '';
   n2 = '';
   resultado = '';
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {
   }
-  sumar()
+  async sumar()
   {
     this.resultado = this.n1 + this.n2;
+    const toast = await this.toastController.create({
+      message:'El resultado es:' + this.resultado,
+      duration : 3000,
+      position : 'middle',
+    });
+    await toast.present();
+    
+    console.log('Hola Mundo 😊❤❤😊');
   }
 }

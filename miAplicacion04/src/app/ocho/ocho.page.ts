a8c2a1f31f4fb4d176f2bf4d60dc1c5ca104ad00
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-08 11:07:35 -0300

    Clase 5

--- miAplicacion04/src/app/ocho/ocho.page.ts
+++ miAplicacion04/src/app/ocho/ocho.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ocho',
@@ -6,19 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ocho.page.scss'],
 })
 export class OchoPage implements OnInit {
-  n1: number = 0;
-  n2: number = 0;
-  n3: number = 0;
+  n1: string = '';
+  n2: string = '';
+  n3: string = '';
   promedio = 0;
   
-  constructor() { }
+  constructor(private alertController:AlertController ) { }
 
   ngOnInit() {
   }
   async promediar()
   {
-    this.promedio = (this.n1 +
-                    this.n2 +
-                    this.n3) / 3;
+    let nota1 = Number("0" + this.n1);
+    let nota2 = Number("0" + this.n2);
+    let nota3 = Number("0" + this.n3);
+
+    if(!nota1 || nota1 < 10 || nota1 > 70)
+      this.mensajeAlerta("El valor de la nota 1 no es valido.");
+    else if(!nota2 || nota2 < 10 || nota2 > 70)
+        this.mensajeAlerta("El valor de la nota 2 no es valido.");
+    else if(!nota3 || nota3 < 10 || nota3 > 70)
+      this.mensajeAlerta("El valor de la nota 3 no es valido.");
+    else{
+      this.promedio = (nota1 + nota2 + nota3) / 3;
+    }
+
+
+
+  }
+  async mensajeAlerta(mensaje:string)
+  {
+    const alerta = await this.alertController.create({
+      header    : "Error",
+      subHeader : "Nota ingresada",
+      message   : mensaje,
+      buttons   : ['Aceptar'],
+    });
+    await alerta.present();
   }
 }

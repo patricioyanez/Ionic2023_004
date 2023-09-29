7e9a17e08fbc53527ae0868de0200e05dc972285
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-29 12:09:01 -0300

    update

--- persistencia004/src/app/quinta/quinta.page.ts
+++ persistencia004/src/app/quinta/quinta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from './crud.service';
 
 @Component({
   selector: 'app-quinta',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quinta.page.scss'],
 })
 export class QuintaPage implements OnInit {
-
-  constructor() { }
+  persona: any = [];
+  constructor(private crud: CrudService) { }
 
   ngOnInit() {
   }
-
+  async agregar()
+  {
+    
+// Ejercicio 3: Validar todos los datos  antes de guardar
+// usar TOAST rojo error y verde guardado.
+    this.crud.guardar(this.persona.rut, this.persona);
+    
+// Ejercicio 4: Terminar el crud para los datos de persona
+  }
 }

57c6b6543e3449e434cca56b4650c3e7c4d0b536
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-10 13:18:38 -0300

    crud

--- persistencia004/src/app/quinta/quinta.page.ts
+++ persistencia004/src/app/quinta/quinta.page.ts
@@ -14,18 +14,26 @@ export class QuintaPage implements OnInit {
 
   ngOnInit() {
   }
-  async agregar()
-  {
-    
+  async guardar()
+  {    
 // Ejercicio 3: Validar todos los datos  antes de guardar
 // usar TOAST rojo error y verde guardado.
     if(!this.persona.rut)
       this.mensajeError("Falta el rut");
     else if(!this.persona.nombre)
       this.mensajeError("Falta el nombre");
+    else if(!this.persona.direccion)
+      this.mensajeError('Falta el Dirección');
+    else if(!this.persona.correo)
+      this.mensajeError('Falta el Correo');
+    else if(!this.persona.telefono)
+      this.mensajeError('Falta el teléfono');
+    else if(!this.persona.anioNacimiento)
+      this.mensajeError('Falta Año de Nacimiento');
     else{
       this.crud.guardar(this.persona.rut, this.persona);
-
+      this.mensajeExito("Persona guardada");
+      this.persona = []; // limpia el form
     }
     
 // Ejercicio 4: Terminar el crud para los datos de persona
@@ -36,8 +44,49 @@ export class QuintaPage implements OnInit {
       message     : mensaje,
       color       : 'danger',
       duration    : 3000,
-      icon        : 'alert'
+      icon        : 'alert-circle-outline',
+      buttons     : ['Aceptar']
     });
     t.present();
+  }
+  async mensajeExito(mensaje:string)
+  {
+    const t = await this.toast.create({
+      message     : mensaje,
+      color       : 'success',
+      duration    : 3000,
+      icon        : 'checkmark-circle-outline',
+      buttons     : ['Aceptar']
+    });
+    t.present();
+  }
+  async leer()
+  {    
+    if(!this.persona.rut)
+      this.mensajeError("Falta el rut");
+    else
+    {
+      await this.crud.leer(this.persona.rut).then( x => {this.persona = x })
+      if(!this.persona)
+      {
+        this.mensajeError("Rut no encontrado");
+        this.persona = [];
+      }
+    }
+  }
+  async eliminar()
+  {    
+    if(!this.persona.rut)
+      this.mensajeError("Falta el rut");
+    else
+    {
+      await this.crud.eliminar(this.persona.rut)
+      this.mensajeExito("Operación finalizada")
+      this.persona = [];      
+    }
+  }
+  limpiar()
+  {
+    this.persona = []; 
   }
 }

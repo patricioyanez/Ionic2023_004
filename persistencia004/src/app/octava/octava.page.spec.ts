3f7978c5af4f2da8681d508b2bb529493674494f
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-10 14:31:33 -0300

    update

--- persistencia004/src/app/octava/octava.page.spec.ts
+++ persistencia004/src/app/octava/octava.page.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { OctavaPage } from './octava.page';
-
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
 /*
 describe('OctavaPage', () => {
   let component: OctavaPage;
@@ -19,4 +21,28 @@ describe('OctavaPage', () => {
 
 */
 
+describe('====== Pagina octava', ()=>{
+  let component: OctavaPage;
+  let fixture: ComponentFixture<OctavaPage>;
 
+  beforeEach(waitForAsync( async()=>{
+    await TestBed.configureTestingModule({
+      declarations: [OctavaPage],
+      imports: [IonicModule.forRoot(), FormsModule, RouterTestingModule ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(OctavaPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }))
+
+  it('______ Botón sumar', fakeAsync(()=>{
+    fixture.componentInstance.n1 = 20
+    fixture.componentInstance.n2 = 20
+    document.getElementById('btnSumar')?.click();
+    fixture.detectChanges();
+    tick();
+    const resultado = (<HTMLInputElement>document.getElementById('resultado')).value;
+    expect(resultado).toContain('40');
+  }))
+
+})
